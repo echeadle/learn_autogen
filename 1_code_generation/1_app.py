7f3956e0965ee6ddce584f954439d95586cbe74d
echeadle <echeadle@gmail.com>
2024-02-17 12:00:45 -0700

    Complted exercies 1

--- 1_code_generation/1_app.py
+++ 1_code_generation/1_app.py
@@ -6,5 +6,100 @@ from IPython.display import display, Image
 import autogen 
 
 confg_list = autogen.config_list_from_json(
-    "OAI_CONFIG_LIST"
+    "OAI_CONFIG_LIST",
 )
+
+# create an AssistantAgent named "assistant"
+assistant = autogen.AssistantAgent(
+    name="assistant",
+    llm_config={
+        "cache_seed": 42,
+        "config_list": confg_list,
+        "temperature": 0
+    },
+)
+# create a UserProxyAgent instance named "user_proxy"
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=10,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={
+        "work_dir": "coding",
+        "use_docker": True
+    }
+)
+# the assistant receives a message from the user_proxy, which contains the task description
+chat_res = user_proxy.initiate_chat(
+    assistant,
+    message="""What is the date today? Compare the year-to-date
+    gain for META and TESLA.""",
+    summary_method="reflection_with_llm",
+)
+
+print("Chat history:", chat_res.chat_history)
+
+print("Summary:", chat_res.summary)
+print("Cost info:", chat_res.cost)
+
+# followup of the previous question
+chat_res = user_proxy.send(
+    recipient=assistant,
+    message="""Plot a chart of their stock price change YTD and save to stock_price_ytd.png.""",
+)
+
+print("Chat history:", chat_res.chat_history)
+print("Summary:", chat_res.summary)
+print("Cost info:", chat_res.cost)
+
+try:
+    image = Image(filename="coding/stock_price_ytd.png")
+    display(image)
+except FileNotFoundError:
+    print("Image not found. Please check the file name and modify if necessary.")
+   
+class IPythonUserProxyAgent(autogen.UserProxyAgent):
+    def __init__(self, name: str, **kwargs):
+        super().__init__(name, **kwargs)
+        self._ipython = get_ipython()
+
+    def generate_init_message(self, *args, **kwargs) -> Union[str, Dict]:
+        return (
+            super().generate_init_message(*args, **kwargs)
+            + """
+If you suggest code, the code will be executed in IPython."""
+        )
+
+    def run_code(self, code, **kwargs):
+        result = self._ipython.run_cell("%%capture --no-display cap\n" + code)
+        log = self._ipython.ev("cap.stdout")
+        log += self._ipython.ev("cap.stderr")
+        if result.result is not None:
+            log += str(result.result)
+        exitcode = 0 if result.success else 1
+        if result.error_before_exec is not None:
+            log += f"\n{result.error_before_exec}"
+            exitcode = 1
+        if result.error_in_exec is not None:
+            log += f"\n{result.error_in_exec}"
+            exitcode = 1
+        return exitcode, log, None
+    
+ipy_user = IPythonUserProxyAgent(
+    "ipython_user_proxy",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=10,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE")
+    or x.get("content", "").rstrip().endswith('"TERMINATE".'),
+    code_execution_config={
+        "use_docker": False,  # Please set use_docker=True if docker is available to run the generated code. Using docker is safer than running the generated code directly.
+    },
+)
+# the assistant receives a message from the user, which contains the task description
+ipy_user.initiate_chat(
+    assistant,
+    message="""Plot a chart of META and TESLA stock price gain YTD""",
+)
+print("Chat history:", chat_res.chat_history)
+print("Summary:", chat_res.summary)
+print("Cost info:", chat_res.cost)
